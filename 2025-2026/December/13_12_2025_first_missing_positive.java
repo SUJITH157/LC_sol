986692e8825cbea6bc098c2138db1cbb0411325c
sujith <reddysujith174@gmail.com>
2025-12-13 15:41:41 +0530

    solved first missed positive number in an array

---
+++ 2025-2026/December/13_12_2025_first_missing_positive.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int smallest = 1;  
+
+        for (int num : nums) {
+            if (num == smallest) {
+                smallest++;
+            }
+        }
+
+        return smallest;
+    }
+}
