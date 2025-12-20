177236779b3c6e12d317f5a1bfda3faab19cb00f
sujith <reddysujith174@gmail.com>
2025-12-20 22:10:04 +0530

    corrected code for set mismatch problem

--- 2025-2026/December/20_12_2025_set_mismatch.java
+++ 2025-2026/December/20_12_2025_set_mismatch.java
@@ -1,16 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        int [] result = new int[2];
-        int i = 0;
-        int j = i +1;
-        for(i=0;i<n;i++){
-            if(nums[i] == nums [j]){
-                result[0] = nums[i];
-                result[1] = nums[i]+1;
+        int[] freq = new int[n + 1];
+        int[] result = new int[2];
+
+        
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+      
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                result[0] = i;   
+            }
+            if (freq[i] == 0) {
+                result[1] = i;   
             }
         }
+
         return result;
-        
     }
 }
