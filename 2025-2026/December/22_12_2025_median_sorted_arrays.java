b4aac8e396702632a86e9bea80b548bee9e05370
sujith <reddysujith174@gmail.com>
2025-12-22 23:06:57 +0530

    solved median of sorted arrays

---
+++ 2025-2026/December/22_12_2025_median_sorted_arrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "empty";
+        }
+
+        StringBuilder commonprefix = new StringBuilder();
+
+        
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return commonprefix.toString(); 
+                }
+            }
+
+            
+            commonprefix.append(c);
+        }
+
+        return commonprefix.toString();
+    }
+}
