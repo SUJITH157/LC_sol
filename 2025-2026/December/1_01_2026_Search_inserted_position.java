1c80fadefb854daaef7a176a066b4c569412f23a
sujith <reddysujith174@gmail.com>
2026-01-01 22:23:14 +0530

    solved search inserted pposition

---
+++ 2025-2026/December/1_01_2026_Search_inserted_position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+            if (nums[i] >= target) {
+                return i;  
+            }
+            i++;
+        }
+
+        
+        return n;
+    }
+}
