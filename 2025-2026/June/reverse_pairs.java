dd406324d25c1939da4c479e099903f7dd87c0cb
sujith <reddysujith174@gmail.com>
2026-06-24 22:42:29 +0530

    solved some leetcode questions

---
+++ 2025-2026/June/reverse_pairs.java
@@ -0,0 +1,71 @@
+class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public int mergeSort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return 0;
+        }
+
+        int mid = (low + high) / 2;
+        int count = 0;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+        count += countPairs(nums, low, mid, high);
+        merge(nums, low, mid, high);
+
+        return count;
+    }
+
+    public int countPairs(int[] nums, int low, int mid, int high) {
+        int right = mid + 1;
+        int count = 0;
+
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) nums[i] > 2L * nums[right]) {
+                right++;
+            }
+            count += (right - (mid + 1));
+        }
+
+        return count;
+    }
+
+    public void merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[k] = nums[left];
+                left++;
+            } else {
+                temp[k] = nums[right];
+                right++;
+            }
+            k++;
+        }
+
+        while (left <= mid) {
+            temp[k] = nums[left];
+            left++;
+            k++;
+        }
+
+        while (right <= high) {
+            temp[k] = nums[right];
+            right++;
+            k++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+    }
+}
