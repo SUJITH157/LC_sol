734df55854119ddc730ff626b848cf2a5a24bf91
sujith <reddysujith174@gmail.com>
2026-07-15 22:20:14 +0530

    solved the middle node value in LL

---
+++ 2025-2026/July 2026/mid_element_LL.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        int count = 0;
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        int mid = count / 2;
+
+        temp = head;
+
+        while (mid > 0) {
+            temp = temp.next;
+            mid--;
+        }
+
+        return temp;
+    }
+}
