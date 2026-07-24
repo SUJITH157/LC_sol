80a95767a491d1952c772c7ef6fd30dc7c32b614
sujith <reddysujith174@gmail.com>
2026-07-24 10:54:24 +0530

    solved a binary consecutive 1 problem in recursion

---
+++ 2025-2026/July 2026/BinaryConsec1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BinaryConsec1 {
+
+    public static ArrayList<String> binaryConsec1(int n) {
+
+        ArrayList<String> result = new ArrayList<>();
+
+        int total = 1 << n;
+
+        for (int i = 0; i < total; i++) {
+
+            String s = Integer.toBinaryString(i);
+
+            while (s.length() < n) {
+                s = "0" + s;
+            }
+
+            if (!isConsecutive(s)) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isConsecutive(String s) {
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == '1' && s.charAt(i - 1) == '1') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
