7a37ad224900ccbdf6a39af7ca170771e1c0abeb
sujith <reddysujith174@gmail.com>
2026-09-05 19:50:36 +0530

    solved a leetcode problem

---
+++ 2025-2026/September/subarrays_k_odds.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        return subsets(nums, k) - subsets(nums, k - 1);
+    }
+
+    public int subsets(int[] nums, int k) {
+
+        if (k < 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int count = 0;
+        int odd = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if (nums[right] % 2 != 0) {
+                odd++;
+            }
+
+            while (odd > k) {
+
+                if (nums[left] % 2 != 0) {
+                    odd--;
+                }
+
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
