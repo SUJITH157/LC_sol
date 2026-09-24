d8f54025547de39440208a690a9eb232a569971d
sujith <reddysujith174@gmail.com>
2026-09-24 09:38:04 +0530

    solved a leetcode problem

---
+++ 2025-2026/September/kth_largest_element_heaps.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+
+    int k;
+    PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            add(nums[i]);
+        }
+    }
+
+    public int add(int val) {
+
+        minHeap.add(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
